68dbc63d0fb231d0d7b0dfeee67ded5d691cf351
Enoch Sem <enochsem@gmail.com>
2022-07-05 22:49:59 +0000

    removed a script tag

---
+++ static/js/homepage.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+Swal.fire('Any fool can use a computer')
+});
+
+
+
+function signup(){
+    $.ajax({
+        type: 'POST',
+        url: '/signup',
+        data: {'device_id': device_id},
+        success: (data) => {
+          console.log("checking " + data['response'][0][3]);
+          var graph_data =  data['response'];
+          for (let index = 0; index < graph_data.length; index++) {
+              if (graph_data[index][2]=="Ph") {
+                  ph_sensor.push({x:graph_data[index][4], y:graph_data[index][3]});
+              }else if(graph_data[index][2]=="Temperature"){
+                  temp_sensor.push({x:graph_data[index][4], y:graph_data[index][3]});
+              }else if(graph_data[index][2]=="Turbidity"){
+                  tur_sensor.push({x:graph_data[index][4], y:graph_data[index][3]});
+              }else if(graph_data[index][2]=="TDS"){
+                  tds_sensor.push({x:graph_data[index][4], y:graph_data[index][3]});
+              }
+          }
+  
+        
+  
+        },
+  
+      });
+}
